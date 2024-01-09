8abfc523cf7ac57edb1126b7e83ffea3b336f12c
indw <caiogmad@gmail.com>
2024-01-09 17:34:37 -0300

    day 2

--- server.js
+++ server.js
@@ -12,7 +12,22 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Rota para a página de login/cadastro
+app.get('/loginCadastro', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'loginCadastro', 'loginCadastro.html'));
+});
+
+// Rota para a página de avaliação/compra
+app.get('/avaliacaoCompra', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'avaliacaoCompra', 'avaliacaoCompra.html'));
+});
+
+// Rota para a página de personalização de usuário
+app.get('/personalizacaoUsuario', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'personalizacaoUsuario', 'personalizacaoUsuario.html'));
+});
+
 // Iniciar o servidor
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://172.16.31.27:${port}`);
 });
