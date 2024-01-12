d695789832fa7e261dc849dacf272831d763b8e7
indw <caiogmad@gmail.com>
2024-01-12 14:21:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 
 const app = express();
-const port = process.env.PORT || 3306;
+const port = process.env.PORT || 3000;
 
 app.use(session({
   secret: 'indw77',
